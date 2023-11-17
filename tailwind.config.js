29212aac634a37d1e0166728ada0f9549588204a
Aryan Soni <arpitsoni811@gmail.com>
2023-11-18 02:24:35 +0530

    content done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: "#ff5710",
+        secondary: "#bababa",
+        accent: "#0634cc",
+        "accent-hover": "#4d76ff",
+      },
+    },
   },
   plugins: [],
 };
